31d290da09d15a62ee20b04a40bc51c3ca419c28
Kenny Z <96443135+Kenny-zhty516@users.noreply.github.com>
2023-10-08 23:31:50 +0000

    add mock & jest file so it recongizes images

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { cleanup, render } from '@testing-library/react'; 
+import { cleanup, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { App } from './app';
-// import { Eats_fish } from './Greetings'
+import { Eats_fish } from './Greetings'
 // Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
 // unmount and cleanup DOM after the test is finished. afterEach(cleanup);
 it('App contains text "hello world"', () => {
@@ -10,8 +10,8 @@ it('App contains text "hello world"', () => {
     expect(queryByText(/hello world/i)).toBeTruthy();
 });
 
-// test('Greeting.js contains an image', () => {
-//     const { container } = render(<Eats_fish />);
-//     const imageElement = container.querySelector('img');
-//     expect(imageElement).toBeInTheDocument();
-// });
+test('Greeting.js contains an image', () => {
+    const { container } = render(<Eats_fish />);
+    const imageElement = container.querySelector('img');
+    expect(imageElement).toBeInTheDocument();
+});
